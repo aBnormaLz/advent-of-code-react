44d510f1a6439110fc2f22d1003d3427521acf24
Sükein Alex <sukein.alex@gmail.com>
2024-07-08 13:18:39 +0200

    rename Day11

--- src/test/year2023/Day11.test.ts
+++ src/test/year2023/Day11.test.ts
@@ -1,7 +1,7 @@
 import { Task } from '../util/Task'
 import { GalaxyMap } from './day11/GalaxyMap'
 
-class Day2 extends Task {
+class Day11 extends Task {
   constructor() {
     super(11, 2023)
   }
@@ -16,26 +16,26 @@ class Day2 extends Task {
 }
 
 test('Part 1 example', () => {
-  const day = new Day2()
+  const day = new Day11()
   const input = day.getExample()
   expect(day.solution(input, 2)).toBe(374)
 })
 
 test('Part 1 task', () => {
-  const day = new Day2()
+  const day = new Day11()
   const input = day.getTask()
   expect(day.solution(input, 2)).toBe(9693756)
 })
 
 test('Part 2 example', () => {
-  const day = new Day2()
+  const day = new Day11()
   const input = day.getExample()
   expect(day.solution(input, 10)).toBe(1030)
   expect(day.solution(input, 100)).toBe(8410)
 })
 
 test('Part 2 task', () => {
-  const day = new Day2()
+  const day = new Day11()
   const input = day.getTask()
   expect(day.solution(input, 1000000)).toBe(717878258016)
 })
